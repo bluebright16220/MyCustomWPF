f24873e448e1f6996adf90b3dcaa76a6edc60409
TeslaKSJ <teslasystemksj@gmail.com>
2019-06-21 16:58:16 +0900

    Create control and dll
    - Create CUDA and build to dll
    - Create simple user control in WPF project
    - Import (CUDA) dll in WPF project

--- MyWPF/MainWindow.xaml.cs
+++ MyWPF/MainWindow.xaml.cs
@@ -11,7 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Runtime.InteropServices;
 using System.Windows.Shapes;
+
 
 namespace MyWPF
 {
@@ -20,9 +22,86 @@ namespace MyWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+
+            phoneDigitPanel.DigitBtnClickEventHandler += receivePhoneBtnDigitClick;
         }
+
+
+        [DllImport("MyCalCulator.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe public static extern void exportCppFunctionAdd(int[] src, int[] src2, int[] dst, int length);
+
+        [DllImport("MyCalCulator.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe public static extern void exportCppFunctionSub(int[] src, int[] src2, int[] dst, int length);
+
+
+        private void receivePhoneBtnDigitClick(object sender, Phone.BasicNumberPanel.DigitBtnEventArgs e)
+        {
+
+            string receivedDigitString = e.digit;
+
+
+            if (string.Compare("*", receivedDigitString) == 0)
+            {
+                //asterisk
+
+                string dialString = txt_dial.Text;
+                int txtLength = dialString.Length;
+
+                if (txtLength > 0)
+                {
+                    int number = int.Parse(dialString);
+
+                    int[] src1 = { number * 10, number * 20 };
+                    int[] src2 = { number, number * 2 };
+                    int[] dst = { 0, 0 };
+
+                    exportCppFunctionAdd(src1, src2, dst, 2);
+
+                    MessageBox.Show(dst[0] + "," + dst[1] + "");
+
+                    dialString = dialString.Substring(0, txtLength - 1);
+                    txt_dial.Text = dialString;
+                }               
+                
+            }
+            else if (string.Compare("#", receivedDigitString) == 0)
+            {
+                //pound
+
+                string dialString = txt_dial.Text;
+                int txtLength = dialString.Length;
+
+                if (txtLength > 0)
+                {
+                    int number = int.Parse(dialString);
+
+                    int[] src1 = {number * 10, number * 20};
+                    int[] src2 = {number,number * 2 };
+                    int[] dst = { 0, 0 };
+
+                    exportCppFunctionSub(src1, src2, dst, 2);
+
+                    MessageBox.Show(dst[0] + "," + dst[1] + "");
+                }
+                
+                txt_dial.Clear();
+            }
+            else
+            {
+                txt_dial.AppendText(e.digit);
+
+            }
+            
+
+            //MessageBox.Show("Your click button is : " + e.digit);
+
+        }
+
+
+
     }
 }
---
+++ MyWPF/Phone/BasicNumberPanel.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace MyWPF.Phone
+{
+    /// <summary>
+    /// MyCalculator.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class BasicNumberPanel : UserControl
+    {
+
+        public event EventHandler<DigitBtnEventArgs> DigitBtnClickEventHandler;
+
+
+        public BasicNumberPanel()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnDigit_Click(object sender, RoutedEventArgs e)
+        {
+            var keyword = (e.Source as Button).Content.ToString();
+            OnBtnDigitClicked(new DigitBtnEventArgs(keyword));
+
+        }
+
+
+        protected virtual void OnBtnDigitClicked(DigitBtnEventArgs e)
+        {
+            DigitBtnClickEventHandler?.Invoke(this, e);
+        }
+
+
+        public class DigitBtnEventArgs : EventArgs
+        {
+
+            public string digit { get; set; }
+
+            public DigitBtnEventArgs(string digit)
+            {
+                this.digit = digit;
+            }
+
+        }
+
+    }
+
+
+}
